ada744d4c402c4d0658c382e0e2d9a3705daa24a
Oleksandr Savchuk <oleksandr.savchuk@lasoft.org>
2024-08-23 19:31:31 +0300

    edit database configs

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,14 +76,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        'ENGINE': 'django.db.backends.postgresql',
-        'HOST': 'localhost',
-        'PORT': '5432',
-        'NAME': 'djangogirls',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
+        # 'NAME': 'djangogirls',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'postgres',
     }
 }
 
